b2e30e899d6ca65d7ed78c1ed865bfdcb9879ca0
Hayley Mentz <hayley.mentz@gmail.com>
2022-07-13 17:38:38 -0700

    enrollment bakend done

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+has_many :enrollments, dependend: :destroy
+has_many :courses, through: :enrollments
 
 validates :first, :last, :image, presence: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 namespace :api do
-  resources :courses
+  resources :courses do
+    resources :enrollments
+  end
   
-  resouces :users
+  resources :users
 end
   # Defines the root path route ("/")
   # root "articles#index"
