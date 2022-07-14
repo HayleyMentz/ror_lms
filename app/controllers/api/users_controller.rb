0d68b72347b674f55a9a0bf2d7684117606d357a
Hayley Mentz <hayley.mentz@gmail.com>
2022-07-14 15:55:27 -0700

    finishing up

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -9,6 +9,14 @@ class Api::UsersController < ApplicationController
     render json: @user
   end
 
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      render json: @user
+    else
+      render json: {errors: @user.errors }, status: :unprocessable_entity
+    end
+
   def update
     if @user.update(course_params)
     else
